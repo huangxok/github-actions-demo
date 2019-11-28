d81dd7cf8d0f0b68a23805805f901d64db1503f1
huangxianwei <huangx.ok@163.com>
2019-11-28 13:28:58 +0800

    fix: update index.html

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          github action demo
         </a>
       </header>
     </div>
