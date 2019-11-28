b3635b257b120329322290eb9b3a02cda70adcb3
huangxok <huangx.ok@163.com>
2019-11-28 06:32:46 +0000

    Deploying to gh-pages from master 33bcd1103db936aa1b2e1f04bfe41d46a8cc3312

---
+++ precache-manifest.95994df1fe7895d9f8fc3df7bbb848b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcdcf7be409ef2e02212f2832eee841d",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "b22cfead9fa50416619c",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d2718bbd4f8fb500d63e",
+    "url": "/github-actions-demo/static/js/2.2f60c060.chunk.js"
+  },
+  {
+    "revision": "b22cfead9fa50416619c",
+    "url": "/github-actions-demo/static/js/main.3a835c2f.chunk.js"
+  },
+  {
+    "revision": "dfb417f264dd525bbb87",
+    "url": "/github-actions-demo/static/js/runtime~main.553754d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
